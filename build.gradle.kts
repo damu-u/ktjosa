e4ac68b9b4d24af45ef958c765bae71a30bffb78
damu <zmmx019@gmail.com>
2025-08-01 17:16:12 +0900

    add pom

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
-group = "dev.damu"
+group = "dev.damu.ktjosa"
 version = "1.0.0"
 
 repositories {
@@ -38,6 +38,35 @@ java {
 }
 
 mavenPublishing {
+    coordinates(group.toString(), project.name, version.toString())
+
+    pom {
+        name.set("ktjosa")
+        description.set("Korean josa particle appender for Kotlin.")
+        url.set("https://github.com/damu-u/ktjosa")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("damu")
+                name.set("BeomSeok Choi")
+                email.set("zmmx019@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/damu-u/ktjosa")
+            connection.set("scm:git:git://github.com/damu-u/ktjosa.git")
+            developerConnection.set("scm:git:ssh://git@github.com/damu-u/ktjosa.git")
+        }
+    }
+
     publishToMavenCentral()
     signAllPublications()
 }
